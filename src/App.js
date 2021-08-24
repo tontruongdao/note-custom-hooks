cdb4c05365ed37358ad3411b87d702f08ce556ed
truongsinistar <truong@sinistar.ca>
2021-08-24 19:37:17 -0400

    added one more custom hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react'
 import useLocalStorage from './useLocalStorage';
+import useUpdateLogger from './useUpdateLogger';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [name, setName] = useLocalStorage('name', '')
+  useUpdateLogger(name)
 
   return (
     <div className="App">
